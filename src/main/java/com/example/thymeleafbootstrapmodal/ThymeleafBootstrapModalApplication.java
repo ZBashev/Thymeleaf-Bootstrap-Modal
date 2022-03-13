ff587fdb0f549b9b98b0e64ccea650069886be5c
Zhuliyan Bashev <64646676+ZBashev@users.noreply.github.com>
2022-03-13 16:56:52 +0100

    Initial commit

---
+++ src/main/java/com/example/thymeleafbootstrapmodal/ThymeleafBootstrapModalApplication.java
@@ -0,0 +1,13 @@
+package com.example.thymeleafbootstrapmodal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymeleafBootstrapModalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThymeleafBootstrapModalApplication.class, args);
+    }
+
+}
